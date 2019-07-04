f6089fbfaf0ac4be5df0bef128d035ad1bb41ce6
toywonder <toy.wonder70@gmail.com>
2019-07-05 08:10:05 +0900

    :sparkles:著者を表示するControllerとViewを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,10 @@ class BooksController < ApplicationController
   require 'natto'
 
   def index
-    @books = Book.all.limit(30)
+    @books = Book.all.limit(30).includes(:author)
   end
 
   def show
     @book = Book.find_by(id: params[:id])
-    # natto = Natto::MeCab.new
-    # @words = natto.enum_parse(@book.sentence)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'books#index'
   resources :books
+  resources :authors
   namespace :api, format: 'json' do
     resources :books
   end
