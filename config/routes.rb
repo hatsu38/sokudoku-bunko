27b741753536770313c470ac67003776cd66d997
toywonder <toy.wonder70@gmail.com>
2019-07-06 14:37:55 +0900

    :art:Bulmaデザインを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'books#index'
-  resources :books
-  resources :authors
+  resources :books, only: %i[index show]
+  resources :authors, only: %i[index show]
   namespace :api, format: 'json' do
-    resources :books
+    resources :books, only: %i[index show]
   end
 end
