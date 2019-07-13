c2946b1f5f72d19df1b7c60da39665d240c782dd
toywonder <toy.wonder70@gmail.com>
2019-07-13 18:46:02 +0900

    :green_heart:ModelのScopeとメソッドのテストを追加

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -32,4 +32,42 @@ RSpec.describe Book, type: :model do
       expect(book.rakuten_book_info).to be_present
     end
   end
+
+  describe "scope" do
+    let!(:books) { create_list(:book, 3) }
+    describe 'recent' do
+      it 'work' do
+        expect(Book.recent(3).count).to eq(3)
+        expect(Book.recent(1).first).to eq books.last
+      end
+    end
+
+    describe 'efficiency_list' do
+      it 'work' do
+        expect(Book.efficiency_list.pluck(:id)).to eq(Book.all.pluck(:id))
+        expect(Book.efficiency_list.pluck(:title)).to eq(Book.all.pluck(:title))
+        expect(Book.efficiency_list.pluck(:author_id)).to eq(Book.all.pluck(:author_id))
+      end
+    end
+  end
+
+  describe 'method' do
+    describe 'search' do
+      let!(:books) { create_list(:book, 3) }
+      let!(:book) { create(:book, title: '羅生門') }
+      context 'exist params' do
+        it 'work' do
+          expect(Book.search('生').count).to eq(1)
+          expect(Book.search('生').first.title).to eq(book.title)
+        end
+      end
+
+      context 'not exist params' do
+        it 'work' do
+          expect(Book.search('').count).to eq(Book.all.count)
+          expect(Book.search('').first.id).to eq(Book.last.id)
+        end
+      end
+    end
+  end
 end
