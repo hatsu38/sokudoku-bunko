c14cf0a0c5d1ae2dfb916e2311e9bc5ebdc2e1f4
toywonder <toy.wonder70@gmail.com>
2019-08-15 08:14:01 +0900

    :bug:Development環境ではログインせずに管理画面を使用可能,キャプションの編集を可能に

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -61,6 +61,6 @@ class Admin::BooksController < AdminController
 
   def book_params
     params.require(:book).permit(:title, :txt_file, :published, :is_published,
-                                 rakuten_book_info_attributes: %i[id price affiliate_url small_image_url medium_image_url])
+                                 rakuten_book_info_attributes: %i[id price caption affiliate_url small_image_url medium_image_url])
   end
 end
