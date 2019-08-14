28c1afc4556e4cb7017deb75f197d51d2305bd1c
toywonder <toy.wonder70@gmail.com>
2019-08-14 19:34:24 +0900

    :bug:Ranking順に正しく表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,12 +17,12 @@ class BooksController < ApplicationController
   end
 
   def ranking
-    @rankings = Book.viewable.where(id: Ranking.order(rank: :asc).select(:book_id)).includes(:author, :rakuten_book_info)
+    @rankings = Book.viewable.joins(:ranking).includes(:author, :rakuten_book_info, :ranking).order('rank')
   end
 
   private
 
   def set_ranking
-    @rankings = Book.viewable.where(id: Ranking.order(rank: :asc).select(:book_id)).limit(12).includes(:author, :rakuten_book_info)
+    @rankings = Book.viewable.joins(:ranking).includes(:author, :rakuten_book_info, :ranking).order('rank').limit(15)
   end
 end
