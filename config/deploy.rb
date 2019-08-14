686b32a612be8cb1ae8dca1272f7b28c5fde6b2a
toywonder <toy.wonder70@gmail.com>
2019-08-14 22:45:39 +0900

    :bug:ProductionのDBはProtectされているので修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,7 @@ namespace :deploy do
     on roles(:app) do
       with rails_env: fetch(:rails_env) do
         within current_path do
-          execute :bundle, :exec, :rake, 'db:migrate:reset'
+          execute :bundle, :exec, :rake, 'db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1'
         end
       end
     end
